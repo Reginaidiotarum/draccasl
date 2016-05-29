f4d58984a6a422a4e2e5ee862c67cd8c57832ffc
edward-tohr <edward.tohr@gmail.com>
2016-05-29 09:22:10 -0400

    updated constants

--- constants.h
+++ constants.h
@@ -6,6 +6,10 @@ class Constants{
 public:
 static const int SCREEN_WIDTH = 640;
 static const int SCREEN_HEIGHT = 480;
+static const int LEVEL_WIDTH = 1024;
+static const int LEVEL_HEIGHT = 1024;
+static const int TILE_WIDTH = 64;
+static const int TILE_HEIGHT = 64;
 
 };
 
